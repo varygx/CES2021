9ad6584ddbf8f507b4ecc5bf9319714129ba80ba
varygx <allenuy0211@gmail.com>
2024-03-10 20:42:52 -0400

    Updated data download instructions

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,27 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from CES 21
+# that the person votes for
+# Author: Allen Uy 
+# Date: 12 March 2023 
+# Contact: allen.uy@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(dataverse)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+ces2021 <- get_dataframe_by_name(
+  filename = "2021 Canadian Election Study v2.0.tab",
+  dataset = "10.7910/DVN/XBZHKC",
+  server = "dataverse.harvard.edu",
+  .f = read_table
+)
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(ces2021, "data/raw_data/raw_ces2021.csv") 
 
          

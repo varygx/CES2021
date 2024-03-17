606e37b93f6e6d9be4b3a88eee0479f5262f335c
varygx <allenuy0211@gmail.com>
2024-03-16 21:31:35 -0400

    Updated simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,12 +1,12 @@
 #### Preamble ####
-# Purpose: Simulates a dataset of predictor variables and the party
+# Purpose: Simulates a dataset of predictor variables and the party. Based on sketches
+# prior to obtaining the actual data
 # that the person votes for
 # Author: Allen Uy 
 # Date: 16 March 2024
 # Contact: allen.uy@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
-
 
 #### Workspace setup ####
 library(tidyverse)
@@ -22,8 +22,10 @@ canada_political_preference <- tibble(
   economy = sample(1:5, size=num_obs, replace=TRUE),
   covid = sample(1:5, size=num_obs, replace=TRUE),
   environment = sample(1:5, size=num_obs, replace=TRUE),
-  party = sample(1:5, size=party, replace=TRUE)
+  support_prob = ((age/10 + education + economy + covid/2 + environment/3)/28)
 )
 
-
+canada_political_preference <- canada_political_preference %>% mutate(
+  voted_for = if_else(runif(n = num_obs) < support_prob, "Liberal", "Conservative")
+) %>% select(-support_prob)
 
